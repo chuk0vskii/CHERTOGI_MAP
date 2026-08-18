81a56ca9f795e43c4f44b1e3edf64dcf28d208a8
chuk0vskii <skatttt666@gmail.com>
2026-08-18 17:39:46 +1200

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -1,1 +1,27 @@
 
+const LOCATIONS = [
+  {
+    id: 1,
+    x: 1500,
+    y: 800,
+    name: 'Штаб синдиката',
+    description: 'Здесь находится главный штаб синдиката. Вход строго по пропускам.',
+    icon: 'assets/icons/sindicat.png'
+  },
+  {
+    id: 2,
+    x: 2800,
+    y: 1200,
+    name: 'Замок графа',
+    description: 'Мрачный замок на скале. Говорят, там водятся призраки.',
+    icon: 'https://cdn-icons-png.flaticon.com/512/542/542558.png'
+  },
+  {
+    id: 3,
+    x: 3800,
+    y: 600,
+    name: 'Деревня Ольховка',
+    description: 'Мирная деревня, где можно купить провизию и отдохнуть.',
+    icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828977.png'
+  }
+];
