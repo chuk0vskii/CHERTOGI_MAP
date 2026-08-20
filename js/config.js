741b214addf5c96455943dfb8ecda891579b2101
chuk0vskii <skatttt666@gmail.com>
2026-08-21 01:39:09 +1200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,8 @@
+// ===== КОНФИГУРАЦИЯ SUPABASE =====
+const SUPABASE_URL = 'https://djieimjwhgjgsjcysceh.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_R2gXWQPVT5trC7cL2BDIpw_Av1QHxDi';
 
+const { createClient } = supabase;
+const _supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+console.log('✅ Supabase подключён');
