95e886270d38297a3f3e0e9449b45dd9d15d65a2
chuk0vskii <skatttt666@gmail.com>
2026-09-07 18:33:42 +1200

    Update event-4-landscapes.js

--- js/events/common/event-4-landscapes.js
+++ js/events/common/event-4-landscapes.js
@@ -6,7 +6,7 @@ export default {
   type: 'landscapes',
   
   render: function(event, helpers) {
-    const { createMultipleBars, createResult, createEffect, getCurrentDifficulty, addArrivalBonus } = helpers;
+    const { createMultipleBars, createResult, createEffect, getCurrentDifficulty } = helpers;
     const difficulty = getCurrentDifficulty();
     let html = '';
     
@@ -22,13 +22,7 @@ export default {
         'all_fail': 'Этот пейзаж угнетает, они получают -1 к Прибытию.'
       };
       html += createEffect(resultType, effects);
-      
-      if (resultType === 'all_or_half_success') {
-        addArrivalBonus(1);
-      }
-      if (resultType === 'half_fail' || resultType === 'all_fail') {
-        addArrivalBonus(-1);
-      }
+      // В render НЕ вызываем addArrivalBonus
     }
     
     return html;
@@ -42,18 +36,22 @@ export default {
     
     let resultType = '';
     let resultText = '';
+    let effects = null;
     
     if (successes >= half) {
       resultType = 'all_or_half_success';
       resultText = 'Успех!';
+      effects = { arrival: 1 };
     } else if (failures >= half) {
       resultType = 'half_fail';
       resultText = 'Провал...';
+      effects = { arrival: -1 };
     } else if (failures === total) {
       resultType = 'all_fail';
       resultText = 'Все провалили!';
+      effects = { arrival: -1 };
     }
     
-    return { resultType, resultText };
+    return { resultType, resultText, effects };
   }
 };
