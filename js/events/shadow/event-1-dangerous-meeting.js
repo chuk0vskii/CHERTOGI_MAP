f2c508fe39711f462b392e668417b0f1e6bbe796
chuk0vskii <skatttt666@gmail.com>
2026-09-07 21:48:01 +1200

    Update event-1-dangerous-meeting.js

--- js/events/shadow/event-1-dangerous-meeting.js
+++ js/events/shadow/event-1-dangerous-meeting.js
@@ -33,15 +33,18 @@ export default {
     const value = values[0] || 0;
     let resultType = '';
     let resultText = '';
+    let effects = null;
     
     if (value >= difficulty) {
       resultType = 'success';
       resultText = 'Успех!';
+      effects = null;
     } else {
       resultType = 'fail';
       resultText = 'Провал...';
+      effects = null;
     }
     
-    return { resultType, resultText };
+    return { resultType, resultText, effects };
   }
 };
